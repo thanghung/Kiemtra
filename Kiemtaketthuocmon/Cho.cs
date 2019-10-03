4c979b76d5362a84a08030c5587a65c7d90a9d67
Thang Hung <nthung942014@gmail.com>
2019-10-03 12:05:14 +0700

    add

---
+++ Kiemtaketthuocmon/Cho.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiemtaketthuocmon
+{
+    class Cho : DongVat
+    {
+        public string gau;
+        public void keu()
+        {
+            for (int i = 1; i < 10; i++)
+                gau += "gau ";
+        }
+    }
+}
---
+++ Kiemtaketthuocmon/Meo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiemtaketthuocmon
+{
+    class Meo : DongVat
+    {
+        public string meo;
+        public void keu()
+        {
+            for(int i=1; i<10; i++)
+            meo += "meo ";
+        }
+    }
+}
